297cf0a641e711224fdca58895f93a6a9e0432da
credibleforce <jamiefrasermcmurray@gmail.com>
2022-01-29 20:17:30 -0800

    :tada: Initial commit

--- laceworkreports/__init__.py
+++ laceworkreports/__init__.py
@@ -2,6 +2,13 @@
 """laceworkreports is a Python cli/package for creating reports from Lacework data."""
 
 import sys
+
+try:
+    from importlib import metadata as importlib_metadata
+except ImportError:
+    # Running on pre-3.8 Python; use importlib-metadata package
+    import importlib_metadata
+
 from importlib import metadata as importlib_metadata
 
 
