8ee9ccd43464ecc0f1c79b63894a317bd7ef0832
credibleforce <jamiefrasermcmurray@gmail.com>
2022-01-29 19:42:38 -0800

    :tada: Initial commit

---
+++ laceworkreports/__init__.py
@@ -0,0 +1,15 @@
+# type: ignore[attr-defined]
+"""laceworkreports is a Python cli/package for creating reports from Lacework data."""
+
+import sys
+from importlib import metadata as importlib_metadata
+
+
+def get_version() -> str:
+    try:
+        return importlib_metadata.version(__name__)
+    except importlib_metadata.PackageNotFoundError:  # pragma: no cover
+        return "unknown"
+
+
+version: str = get_version()
