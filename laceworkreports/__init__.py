c3bab32f19953e0f6dcd9eb9bd10555ee8f9e679
credibleforce <jamiefrasermcmurray@gmail.com>
2022-01-29 20:20:39 -0800

    :tada: Initial commit

--- laceworkreports/__init__.py
+++ laceworkreports/__init__.py
@@ -9,8 +9,6 @@ except ImportError:
     # Running on pre-3.8 Python; use importlib-metadata package
     import importlib_metadata
 
-from importlib import metadata as importlib_metadata
-
 
 def get_version() -> str:
     try:
