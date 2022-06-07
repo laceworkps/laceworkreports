e7b8b6f25bb5945d7e98532e84dadeda634ba880
credibleforce <jamiefrasermcmurray@gmail.com>
2022-06-07 08:24:27 -0700

    update fixable filter to match package active

--- laceworkreports/sdk/ReportHelpers.py
+++ laceworkreports/sdk/ReportHelpers.py
@@ -1884,10 +1884,6 @@ class ReportHelper:
         result: typing.Any = []
 
         try:
-            fixable_val = 0
-            if fixable:
-                fixable_val = 1
-
             if severity.value == ReportSeverityTypes.CRITICAL.value:
                 severity_types = ["Critical"]
             elif severity.value == ReportSeverityTypes.HIGH.value:
@@ -1910,12 +1906,16 @@ class ReportHelper:
                     "expression": "in",
                     "values": severity_types,
                 },
-                {
-                    "field": "fixInfo.fix_available",
-                    "expression": "eq",
-                    "value": fixable_val,
-                },
             ]
+
+            if fixable:
+                filters.append(
+                    {
+                        "field": "fixInfo.fix_available",
+                        "expression": "eq",
+                        "value": "1",
+                    }
+                )
 
             if package_active:
                 filters.append(
@@ -2081,10 +2081,6 @@ class ReportHelper:
                 if len(image_ids) > 0:
                     logging.info("Retrieving active container vulnerabilities...")
 
-                    fixable_val = 0
-                    if fixable:
-                        fixable_val = 1
-
                     if severity.value == ReportSeverityTypes.CRITICAL.value:
                         severity_types = ["Critical"]
                     elif severity.value == ReportSeverityTypes.HIGH.value:
@@ -2108,12 +2104,16 @@ class ReportHelper:
                             "expression": "in",
                             "values": ["VULNERABLE"],
                         },
-                        {
-                            "field": "fixInfo.fix_available",
-                            "expression": "eq",
-                            "value": fixable_val,
-                        },
                     ]
+
+                    if fixable:
+                        filters.append(
+                            {
+                                "field": "fixInfo.fix_available",
+                                "expression": "eq",
+                                "value": "1",
+                            }
+                        )
 
                     if namespace is not None:
                         filters.append(
